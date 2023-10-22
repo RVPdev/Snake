52d44a76a7df7d3c320390e9924b522af997a1e5
RVPdev <renatovilcahuaman@gmail.com>
2023-10-21 23:41:36 -0500

    initialized initial screen

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,19 @@
 
 int main()
 {
+    // Initialiazing the screen paramaters
+    InitWindow(750, 750, "Retro Snake");
+
+    SetTargetFPS(60);
+
+    while (WindowShouldClose() == false)
+    {
+        BeginDrawing();
+
+        EndDrawing();
+    };
+
+    CloseWindow();
+
     return 0;
 }
