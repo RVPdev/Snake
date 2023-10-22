763107eba89e91904e2f795d195bb692e786a6e7
RVPdev <renatovilcahuaman@gmail.com>
2023-10-22 00:03:42 -0500

    created food class and draw on screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,36 @@
 #include <raylib.h>
 
-Color green = {173,204,96,255};
-Color darkGreen = {43,51,24,255};
+Color green = {173, 204, 96, 255};
+Color darkGreen = {43, 51, 24, 255};
+
+int cellSize = 30;
+int cellCount = 25;
+
+class Food
+{
+public:
+    Vector2 position = {5, 6};
+
+    void Draw()
+    {
+        DrawRectangle(position.x * cellSize, position.y * cellSize, cellSize, cellSize, darkGreen);
+    }
+};
 
 int main()
 {
     // Initialiazing the screen paramaters
-    InitWindow(750, 750, "Retro Snake");
-
+    InitWindow(cellSize * cellCount, cellSize * cellCount, "Retro Snake");
     SetTargetFPS(60);
+
+    Food food = Food();
 
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
 
-        ClearBackground(green);  // Painting screen background green
+        ClearBackground(green); // Painting screen background green
+        food.Draw();
 
         EndDrawing();
     };
